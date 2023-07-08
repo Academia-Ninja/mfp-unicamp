2167835dd0a306ead2938ab8d0ccb0d756981ec4
Marlene Moraes <marlenevmoraes@outlook.com>
2023-07-08 17:38:26 -0300

    exercise intermediate #3

---
+++ mfp-class/intermediate-class/codeforces_874.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+// CodeForces Round #874 (Div. 3)
+void print_874(){
+  int w;
+  cin >> w;
+
+  if(w % 2 == 0 && w >= 4){
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+}
